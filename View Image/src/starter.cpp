358e510a8bb657fb09904d7c301e6ab0ddfad766
z3dxz <3dxz2x@gmail.com>
2023-08-16 22:09:13 -0400

    removed test

--- View Image/src/starter.cpp
+++ View Image/src/starter.cpp
@@ -92,7 +92,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 				gp.iLocX -= 3;
 				RedrawImageOnBitmap(&gp);
 			}
-			RedrawImageOnBitmap(&gp);
 		}
 
 		MSG msg = { 0 };
